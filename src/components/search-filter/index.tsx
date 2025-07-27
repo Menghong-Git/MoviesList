9cf6c162692cff4062b671084664e5b608a1a768
Pen Menghong <penmenghong7@gmail.com>
2025-07-27 13:07:25 +0700

    Save local changes before merge

---
+++ src/components/search-filter/index.tsx
@@ -0,0 +1,39 @@
+
+// import { styled, alpha } from '@mui/material/styles';
+// import InputBase from '@mui/material/InputBase';
+
+
+// const Search = styled('div')(({ theme }) => ({
+//   position: 'relative',
+//   borderRadius: theme.shape.borderRadius,
+//   backgroundColor: alpha(theme.palette.common.white, 0.15),
+//   '&:hover': {
+//     backgroundColor: alpha(theme.palette.common.white, 0.25),
+//   },
+//   marginLeft: 0,
+//   width: '100%',
+//   [theme.breakpoints.up('sm')]: {
+//     marginLeft: theme.spacing(1),
+//     width: 'auto',
+//   },
+// }));
+
+
+
+// const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//   color: 'inherit',
+//   width: '100%',
+//   '& .MuiInputBase-input': {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     // vertical padding + font size from searchIcon
+//     transition: theme.transitions.create('width'),
+//     [theme.breakpoints.up('sm')]: {
+//       width: '200ch',
+//       '&:focus': {
+//         width: '200ch',
+//       },
+//     },
+//   },
+// }));
+
+// export { Search, StyledInputBase };
